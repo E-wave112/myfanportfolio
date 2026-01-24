cc45f6679a33f204b9ee623b73e19ce36374b15f
E-wave112 <osagieiyayi09@gmail.com>
2026-01-24 16:14:24 +0100

    chore: Install all dependencies and setup the design system with Tailwind CSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Legacy files (to be removed after migration)
+    "js/**",
+    "css/**",
   ]),
 ]);
 
